990733639938d48178711e54ba2f2526a078b6b1
dot-ammar <80134790+dot-ammar@users.noreply.github.com>
2024-10-23 20:32:34 -0400

    new

--- SonarEmbeddingFunction.py
+++ SonarEmbeddingFunction.py
@@ -2,8 +2,13 @@ from chromadb import Documents, EmbeddingFunction, Embeddings
 from sonar.inference_pipelines.text import TextToEmbeddingModelPipeline
 import torch 
 
-device = torch.device("mps")
-
+if torch.backends.mps.is_available():
+    device = torch.device("mps")
+    x = torch.ones(1, device=device)
+    print (x)
+else:
+    print ("MPS device not found.")
+    
 class SonarEmbeddingFunction(EmbeddingFunction):
     def __init__(self):
         # Initialize the model once when the class is instantiated
@@ -12,10 +17,13 @@ class SonarEmbeddingFunction(EmbeddingFunction):
             tokenizer="text_sonar_basic_encoder",
             device=device
         )
-
+        self.max_token_length = 512
+    
     def __call__(self, input: Documents) -> Embeddings:
         # Reuse the already initialized model
         embeddings = self.t2vec_model.predict(input, source_lang="eng_Latn")        
         embeddings_as_list = embeddings.tolist()
         
         return embeddings_as_list
+    
+    
